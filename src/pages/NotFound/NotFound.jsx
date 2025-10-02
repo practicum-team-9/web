488e2ae9504e8ca475d4894ead26ce50d6f936d2
DKSolo <dbm95mail@gmail.com>
2025-10-02 22:22:58 +0500

    feat: page 404 style-less

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import { FormsTable } from "@widgets";
+import { Template } from "@widgets";
+import image404 from "../../assets/images/404.png";
 
 function NotFound() {
 
   return (
-    <>404 NotFound</>
+    <>
+        <Template>
+            <div className="notFound">
+                <div className="imageContainer">
+                    <img src={image404} alt="Страница с ошибкой 404" />
+                </div>
+                <div className="textContainer"> 
+                    <h2>Ой, а такой страницы нет!</h2>
+                    <p>Вы можете вернуться на <a href='/choose-form'>страницу выбора форм</a></p>
+                </div>
+            </div>
+        </Template>
+    </>
   );
 }
 
