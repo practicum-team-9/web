9df07a75adfbf3352da1dd032464ff6bdf4affb9
DKSolo <dbm95mail@gmail.com>
2025-11-13 11:41:54 +0500

    feat: 404 adaptive styling

--- src/widgets/NotFoundWidget/NotFoundWidget.jsx
+++ src/widgets/NotFoundWidget/NotFoundWidget.jsx
@@ -7,19 +7,21 @@ function NotFoundWidget() {
       <>
         <div className="not-found">
           <div className="image-container">
-            <img src={image404} alt="Страница с ошибкой 404" />
+            <img className="image" src={image404} alt="Страница с ошибкой 404" />
           </div>
           <div className="text-container">
             <h2 className="not-found-h">Ошибка!</h2>
-            <p className="not-found-p">
-              К сожалению такой страницы не существует.{" "}
-            </p>
-            <p className="not-found-p">
-              Вы можете вернуться на{" "}
-              <a className="not-found-a" href="/choose-form">
-                страницу выбора форм
-              </a>
-            </p>
+            <div>
+              <p className="not-found-p">
+                К сожалению такой страницы не существует.{" "}
+              </p>
+              <p className="not-found-p">
+                Вы можете вернуться на{" "}
+                <a className="not-found-a" href="/choose-form">
+                  страницу выбора форм
+                </a>
+              </p>
+            </div>
           </div>
         </div>
         <img
