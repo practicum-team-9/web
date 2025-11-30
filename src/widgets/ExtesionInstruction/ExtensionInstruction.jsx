da2743fd1c2bd443e55ef8422efa9c1d732e339d
DKSolo <dbm95mail@gmail.com>
2025-11-30 15:49:24 +0500

    feat: YaBrowser Instruction Page

--- src/widgets/ExtesionInstruction/ExtensionInstruction.jsx
+++ src/widgets/ExtesionInstruction/ExtensionInstruction.jsx
@@ -1,6 +1,13 @@
 import extensionsImage from "../../assets/images/extensionsImg.png";
-// import yandexBrowserIcon from "../../assets/images/YandexBrowserIcon.png";
-import googleChromeIcon from "../../assets/images/GoogleChromeIcon.png";
+import yandexBrowserIcon from "../../assets/images/YandexBrowserIcon.png";
+
+import step1 from "../../assets/images/step1.png";
+import step2 from "../../assets/images/step2.png";
+import step3 from "../../assets/images/step3.png";
+import step4 from "../../assets/images/step4.png";
+import step5 from "../../assets/images/step5.png";
+
+// import googleChromeIcon from "../../assets/images/GoogleChromeIcon.png";
 import './ExtensionInstruction.css'
 // import { useNavigate } from 'react-router-dom';
 
@@ -10,34 +17,36 @@ function ExtensionInstruction() {
   return (
       <div className="instructions-container">
         <div className="instructions-header">
-          <img
-            src={googleChromeIcon}
-            className="button-image"
-            alt="Иконка Google Chrome"
-          />
-          <h2>Как установить расширение?</h2>
+          <div className="instructions-logo">
+            <img
+              src={yandexBrowserIcon}
+              className="logo-img"
+              alt="Иконка Яндекс Браузер"
+            />
+          </div>
+          <h1 className="instructions-h1">Как установить расширение?</h1>
         </div>
         <div className="instructions-body">
           <ul className="instructions-steps">
             <li className="instructions-step">
               <p className="step-text">1. Нажмите на скачанный файл, чтобы разархивировать его</p>
-              <img className="step-img" src={extensionsImage}/>
+              <img className="step-img" src={step1}/>
             </li>
             <li className="instructions-step">
               <p className="step-text">2. В своем браузере введите в строку поиска browser://extensions/ и нажмите кнопку “Enter”</p>
-              <img className="step-img" src={extensionsImage}/>
+              <img className="step-img" src={step2}/>
             </li>
             <li className="instructions-step">
               <p className="step-text">3. Выберите Developer mode, чтобы перейти в режим разработки</p>
-              <img className="step-img" src={extensionsImage}/>
+              <img className="step-img" src={step3}/>
             </li>
             <li className="instructions-step">
               <p className="step-text">4. Нажмите на кнопку “Load unpacked” и выберите разархивированный файл</p>
-              <img className="step-img" src={extensionsImage}/>
+              <img className="step-img" src={step4}/>
             </li>
             <li className="instructions-step">
               <p className="step-text">5. Расширение установлено!</p>
-              <img className="step-img" src={extensionsImage}/>
+              <img className="step-img" src={step5}/>
             </li>
           </ul>
         </div>
