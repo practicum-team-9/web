1c42c531a1fd48a8f160d45ca50c78e689f3a5a0
DKSolo <dbm95mail@gmail.com>
2025-12-14 22:05:07 +0500

    fix: minor cleanUp

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,10 +1,9 @@
-import { useLocation, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 import { NotFoundWidget, Primary } from "@widgets";
 import Loader from "@/shared/ui/Loader/Loader.jsx";
 
 function Landing({ getFormById }) {
-  // const location = useLocation();
   const [form, setForm] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const { id } = useParams()
@@ -15,7 +14,7 @@ function Landing({ getFormById }) {
       .then((form) => setForm(form.data))
       .catch((err) => console.log(err))
       .finally(() => setIsLoading(false));
-  }, [location.pathname]);
+  }, []);
 
   if (isLoading) return <Loader />;
   if (!form) return <NotFoundWidget />;
