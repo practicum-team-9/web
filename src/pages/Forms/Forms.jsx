df10f8d249b7143f4bff182d5509d3b02a8ee6b7
DKSolo <dbm95mail@gmail.com>
2025-12-15 18:10:04 +0500

    fix: Loader for Forms List

--- src/pages/Forms/Forms.jsx
+++ src/pages/Forms/Forms.jsx
@@ -3,13 +3,23 @@ import { FormsList } from "@widgets";
 
 function Forms({ getForms }) {
   const [forms, setForms] = useState([]);
+  const [ isLoading, setIsLoading ] = useState(true)
 
   useEffect(() => {
+    setIsLoading(true)
     getForms()
-      .then((forms) => setForms(forms.data))
-      .catch((err) => console.log(err));
+      .then((forms) => {
+        setForms(forms.data)
+      })
+      .catch((err) => console.log(err))
+      .finally(() => {
+        setIsLoading(false)
+      });
   }, [getForms]);
 
+  if (isLoading) {
+    return <Loader />
+  }
   return <FormsList forms={forms} />;
 }
 
